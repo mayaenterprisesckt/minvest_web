f7d77ae5587b597d13ac8d859b34ed8d5969c45d
zax4r0 <zax4r0@gmail.com>
2021-12-29 14:53:21 +0530

    Updates

---
+++ assets/AboutPage.37d6fc05.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor.cd9ab1c2.js";function m(){return r("div",{children:"hmmmm w"})}export{m as default};
---
+++ assets/ServicesPage.f718d720.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor.cd9ab1c2.js";function i(){return e("div",{children:"Gib all ur service"})}export{i as default};
